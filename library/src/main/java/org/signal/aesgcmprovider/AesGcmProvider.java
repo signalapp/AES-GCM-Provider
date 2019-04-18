b2e54c1335d6c2eff2a0eb27749ee0634eb36eae
Moxie Marlinspike <moxie+github@signal.org>
2019-04-17 18:16:07 -0700

    Load native code in provider

--- library/src/main/java/org/signal/aesgcmprovider/AesGcmProvider.java
+++ library/src/main/java/org/signal/aesgcmprovider/AesGcmProvider.java
@@ -4,6 +4,10 @@ import java.security.Provider;
 
 public final class AesGcmProvider extends Provider {
 
+  static {
+    System.loadLibrary("aesgcm");
+  }
+
   public AesGcmProvider() {
     super("AesGcmProvider", 1.0, "AES GCM BoringSSL-backed provider to work around decisions made by Conscrypt");
     put("Cipher.AES/GCM/NoPadding", "org.signal.aesgcmprovider.AesGcmCipher");
